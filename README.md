7e1c0553b992124c33af1b548d1e6f2040fed583
Kosmostars7403 <3699818@mail.ru>
2020-06-17 10:56:41 +0300

    Add some improvements

--- README.md
+++ README.md
@@ -12,10 +12,22 @@
 $ pip install -r requirements.txt
 ```
 
-Чтобы начать постин, в каждой соц сети придется получить токен.
+Чтобы начать постинг, в каждой соц сети придется получить токен.
 1. для телеграмм [инструкция](https://smmplanner.com/blog/otlozhennyj-posting-v-telegram/)
 2. для VK [инструкция](https://devman.org/qna/63/kak-poluchit-token-polzovatelja-dlja-vkontakte/)
 3. для Facebook [инструкция](https://developers.facebook.com/docs/graph-api/explorer/)
+
+После всех мучений с получением токенов создайте файл .env и заполните следующим образом:
+VK_USER_TOKEN=ваш_токен_ВК
+VK_USER_LOGIN=ваш_логин_ВК
+VK_GROUP_ID=id_вашей_группы_ВК(цифры из ссылки на группу)
+VK_ALBUM_ID=id_альбома_вашей_группы_ВК(цифры из ссылки на альбом группы вк)
+
+TELEGRAM_TOKEN=токен_телеграм_бота
+TELEGRAM_CHANEL_ID=ссылка_на_канал(например: @my_channel_is_awesome)
+
+FACEBOOK_TOKEN=токен_фейсбук
+FACEBOOK_GROUP_ID=id_группы_фейсбук(цифры из ссылки на группу)
 
 ### Пример запуска
 Скрипт принимает на вход два обязательных аргумента, путь до изображения и путь до текстового файла с самим постом(*.txt)
@@ -23,14 +35,14 @@ $ pip install -r requirements.txt
 $python3 vk_tg_fb_posting.py image.jpg text.txt
 ```
 
-Используя дополнительные аругменты, вы можете пропустить постинг в определенную соцсеть.
-1. `-sf` или `--skip_facebook` - пропустить Facebook
-2. `-sv` или `--skip_vk` - пропустить Вконтакте
-3. `-st` или `--skip_telegram` - пропустить Телеграм
+Используя дополнительные аргументы, вы можете пропустить постинг в определенную соцсеть.
+1. `-pf` или `--post_facebook` - выложить в Facebook
+2. `-pv` или `--post_vk` - выложить во Вконтакте
+3. `-pt` или `--post_telegram` - выложить в Телеграм
 
 Пример:
 ```
-$python3 vk_tg_fb_posting.py image.jpg text.txt -sf -sv
+$python3 vk_tg_fb_posting.py image.jpg text.txt -pt
 ```
 Такой ввод запустит скрипт на постинг только в телеграм!
 
--- vk_tg_fb_posting.py
+++ vk_tg_fb_posting.py
@@ -12,43 +12,45 @@ def post_vk(image_path, text_path):
 
     upload = vk_api.VkUpload(vk_session)
 
-    photo = upload.photo(
+    photo_sizes = upload.photo(
         image_path,
-        album_id=272857318,
-        group_id=195764581
+        album_id=vk_album_id,
+        group_id=vk_group_id
     )
+
     with open(text_path, 'r') as caption:
         vk.wall.post(owner_id='-{}'.format(vk_group_id), message=caption.read(), from_group=1,
-                     attachments='photo-{}_{}'.format(vk_group_id, photo[0]['id']))
+                     attachments='photo-{}_{}'.format(vk_group_id, photo_sizes[0]['id']))
 
 
 def post_telegram(image_path, text_path):
     bot = telegram.Bot(token=telegram_token)
-    with open(text_path, 'r') as caption:
-        bot.send_photo(chat_id=telegram_channel, photo=open(image_path, 'rb'))
+    with open(text_path, 'r') as caption, open(image_path, 'rb') as image:
         bot.send_message(chat_id=telegram_channel, text=caption.read())
+        bot.send_photo(chat_id=telegram_channel, photo=image)
 
 
 def post_facebook(image_path, text_path):
     url = "https://graph.facebook.com/v7.0/{}/photos".format(facebook_group_id)
-    files = {'file': open(image_path, 'rb')}
-    with open(text_path, 'r') as caption:
+    with open(text_path, 'r') as caption, open(image_path, 'rb') as image:
+        files = {'file': image}
         payrole = {
             'caption': caption.read(),
             'access_token': facebook_token,
         }
         response = requests.post(url, files=files, data=payrole)
+        response.raise_for_status()
 
 
 def parse_console_arguments():
     parser = argparse.ArgumentParser(description='Постинг в VK, Telegram, Facebook')
     parser.add_argument('image', help='Путь к изображению')
     parser.add_argument('text', help='Путь к тексту')
-    parser.add_argument('-sf', '--skip_facebook', help='Не выкладывать в Facebook.', action='store_false')
-    parser.add_argument('-st', '--skip_telegram', help='Не выкладывать в Telegram.', action='store_false')
-    parser.add_argument('-sv', '--skip_vk', help='Не выкладывать во Вконтакте.', action='store_false')
-    args = parser.parse_args()
-    return args
+    parser.add_argument('-pf', '--post_facebook', help='Не выкладывать в Facebook.', action='store_true')
+    parser.add_argument('-pt', '--post_telegram', help='Не выкладывать в Telegram.', action='store_true')
+    parser.add_argument('-pv', '--post_vk', help='Не выкладывать во Вконтакте.', action='store_true')
+
+    return parser
 
 
 if __name__ == '__main__':
@@ -57,6 +59,7 @@ if __name__ == '__main__':
     vk_user_token = os.getenv('VK_USER_TOKEN')
     vk_user_login = os.getenv('VK_USER_LOGIN')
     vk_group_id = os.getenv('VK_GROUP_ID')
+    vk_album_id = os.getenv('VK_ALBUM_ID')
 
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     telegram_channel = os.getenv('TELEGRAM_CHANEL_ID')
@@ -64,13 +67,15 @@ if __name__ == '__main__':
     facebook_token = os.getenv('FACEBOOK_TOKEN')
     facebook_group_id = os.getenv('FACEBOOK_GROUP_ID')
 
-    args = parse_console_arguments()
+    parser = parse_console_arguments()
 
-    if args.skip_facebook:
+    args = parser.parse_args()
+
+    if args.post_facebook:
         post_facebook(args.image, args.text)
 
-    if args.skip_vk:
+    if args.post_vk:
         post_vk(args.image, args.text)
 
-    if args.skip_telegram:
+    if args.post_telegram:
         post_telegram(args.image, args.text)
